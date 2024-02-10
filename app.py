93832de7fe59bb43825e127ddb2ddecb9c7ec8e0
VictorB1996 <baranga.victor96@gmail.com>
2024-02-10 17:38:08 +0200

    add rest of changes

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 import os
 import shutil
 
-from utils.images import get_random_image, get_difficulty_width_height, resize_image, get_image_size
+from utils.images import (
+    get_random_image,
+    get_difficulty_width_height,
+    resize_image,
+    get_image_size,
+)
 from utils.directions import (
     get_coordinates,
     calculate_direction,
@@ -21,6 +26,7 @@ images_dir = os.path.join(base_path, "static", "images")
 
 user_picked_countries = []
 
+
 def move_images():
     images_dir = os.path.join(base_path, "images")
     static_dir = os.path.join(base_path, "static")
@@ -36,70 +42,102 @@ def static_files(filename):
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        # handle difficulty from request params here
         difficulty = request.args.get("difficulty")
         if not difficulty or difficulty not in allowed_difficulties:
             difficulty = "medium"
 
         width, height = get_difficulty_width_height(difficulty)
-        
-    
-        allowed_guesses = 6
-        current_guesses = 0
 
         country, flag, parts = get_random_image(images_dir, difficulty)
-        session["country"], session["flag"], session["parts"] = country, flag, parts
-        session["allowed_guesses"] = allowed_guesses
-        session["current_guesses"] = current_guesses
-        session["countries"] = os.listdir(images_dir)
-
-
         country_latitiude, country_longitude = get_coordinates(country)
+
         session.update(
             {
+                "country": country,
+                "flag": flag,
+                "parts": parts,
+                "countries": os.listdir(images_dir),
+                "allowed_guesses": len(parts),
+                "current_guesses": 1,
+                "game_over": False,
                 "country_latitiude": country_latitiude,
                 "country_longitude": country_longitude,
             }
         )
 
         # Resize background image as per flag parts
-        background_picture_path = os.path.join(images_dir, "..", "background", "part_background.png")
-        background_new_width, background_new_height = get_image_size(os.path.join(images_dir, country, difficulty, "sub_image_1.png"))
-        resize_image(background_new_width, background_new_height, background_picture_path)
+        background_picture_path = os.path.join(
+            images_dir, "..", "background", "part_background.png"
+        )
+        background_new_width, background_new_height = get_image_size(
+            os.path.join(images_dir, country, difficulty, "sub_image_1.png")
+        )
+        resize_image(
+            background_new_width, background_new_height, background_picture_path
+        )
 
     else:
         user_selected_country = request.form.get("selected-country")
-        user_selected_country_latitude, user_selected_country_longitude = (
-            get_coordinates(user_selected_country)
-        )
 
-        distance = calculate_distance(
-            (session.get("country_latitiude"), session.get("country_longitude")),
-            (user_selected_country_latitude, user_selected_country_longitude),
-        )
+        response = {
+            "distance": None,
+            "direction_png_src": None,
+            "user_selected_country": user_selected_country,
+            "user_guessed_correctly": False,
+            "info_text": "",
+        }
 
-        distance = "{:,}".format(int(distance))
-        distance = str(distance) + " Km"
+        if user_selected_country == session.get("country"):
+            response.update(
+                {
+                    "user_guessed_correctly": True,
+                    "info_text": "You guessed correctly - The answer is {}! Retry below.".format(
+                        session.get("country")
+                    ),
+                }
+            )
+        else:
+            print("Current Guesses: {}".format(session.get("current_guesses")))
+            if session.get("current_guesses") >= len(session.get("parts")):
+                response.update(
+                    {
+                        "game_over": True,
+                        "info_text": "You failed to guess the country - The answer was {}. Retry below.".format(
+                            session.get("country")
+                        ),
+                    }
+                )
+            else:
+                session["current_guesses"] += 1
 
-        direction = calculate_direction(
-            user_selected_country_latitude,
-            user_selected_country_longitude,
-            session.get("country_latitiude"),
-            session.get("country_longitude")
-        )
+                user_selected_country_latitude, user_selected_country_longitude = (
+                    get_coordinates(user_selected_country)
+                )
 
-        direction_png_src = get_direction_png(direction)
+                distance = calculate_distance(
+                    (
+                        session.get("country_latitiude"),
+                        session.get("country_longitude"),
+                    ),
+                    (user_selected_country_latitude, user_selected_country_longitude),
+                )
 
-        return jsonify(
-            {
-                "user_selected_country": user_selected_country,
-                "distance": distance,
-                "direction_png_src": direction_png_src
-            }
-        )
+                distance = "{:,}".format(int(distance))
+                distance = str(distance) + " Km"
 
-    # Reset session when the game ends
-    # (user either guesses the flag or runs out of choices)
+                direction = calculate_direction(
+                    user_selected_country_latitude,
+                    user_selected_country_longitude,
+                    session.get("country_latitiude"),
+                    session.get("country_longitude"),
+                )
+
+                direction_png_src = get_direction_png(direction)
+                response.update(
+                    {"distance": distance, "direction_png_src": direction_png_src}
+                )
+
+        return jsonify(response)
 
     return render_template(
         "index.html",
@@ -108,9 +146,9 @@ def index():
         parts=session["parts"],
         countries=session["countries"],
         user_picked_countries=user_picked_countries,
-        difficulty = difficulty,
+        difficulty=difficulty,
         difficulty_columns=width,
-        difficulty_rows=height
+        difficulty_rows=height,
     )
 
 
--- static/script/change_image.js
+++ static/script/change_image.js
@@ -3,17 +3,21 @@ function getRandomElement(nodeList) {
     return array[Math.floor(Math.random() * array.length)];
 }
 
-function updateImageSource(country, difficulty) {
+function updateFlagPartImageSource(imageElement, country, difficulty) {
+    var elementClasses = Array.from(imageElement.classList);
+    var imagePath = 'static/images/' + country + "/" + difficulty + "/" + elementClasses[1] + ".png";
+    imageElement.src = imagePath
+    imageElement.classList.remove(elementClasses[1]);
+}
+
+function handlePostRequest(country, difficulty) {
     var dropdown = document.getElementById("country-dropdown");
     var selectedCountry = dropdown.value;
 
     var flag_part_imgs = document.querySelectorAll('img[class*="sub_image_"]');
 
     random_flag_part = getRandomElement(flag_part_imgs);
-    classes = Array.from(random_flag_part.classList);
-    image_path = 'static/images/' + country + "/" + difficulty + "/" + classes[1] + ".png";
-    random_flag_part.src = image_path;
-    random_flag_part.classList.remove(classes[1]);
+    updateFlagPartImageSource(random_flag_part, country, difficulty)
 
     dropdown.options[dropdown.selectedIndex].disabled = true;
 
@@ -29,40 +33,94 @@ function updateImageSource(country, difficulty) {
             handleUserGuess(
                 response.user_selected_country,
                 response.distance,
-                response.direction_png_src
+                response.direction_png_src,
+                response.user_guessed_correctly,
+                country,
+                difficulty,
+                response.info_text,
+                response.game_over
             )
         }
     };
 }
 
-function handleUserGuess(pickedCountry, distance, directionPictureSrc) {
-    // Enable the table
-    var table = document.getElementById("user-guesses");
-    table.style.display = "table";
+function showUserText(infoText) {
+    var infoTextElem = document.getElementById("user-info");
+    var optionsDiv = document.querySelector(".options");
 
-    table = table.getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow(table.rows.length);
+    infoTextElem.style.opacity = 0;
+    void infoTextElem.offsetWidth;
 
-    var cell1 = newRow.insertCell(0);
-    var cell2 = newRow.insertCell(1);
+    setTimeout(function () {
+        infoTextElem.textContent = infoText;
+        infoTextElem.style.opacity = 1;
+    }, 10);
 
-    cell1.innerHTML = pickedCountry;
-    cell2.innerHTML = distance;
+    var options = ["easy", "medium", "hard"]
+    for (var i = 0; i <= 2; i++) {
+        var anchorTag = document.createElement('a');
+        anchorTag.textContent = options[i];
+        anchorTag.href = '?difficulty=' + encodeURIComponent(options[i]);
+        optionsDiv.appendChild(anchorTag);
+    }
 
-    cell1.style.textAlign = "center";
-    cell1.style.verticalAlign = "middle";
+    setTimeout(function () {
+        infoTextElem.classList.add("fade-in");
+        optionsDiv.classList.add('fade-in');
+    }, 20);
+}
 
-    cell2.style.textAlign = "center";
-    cell2.style.verticalAlign = "middle";
 
-    var cell3 = newRow.insertCell(2);
-    var directionImage = document.createElement("img");
-    directionImage.src = directionPictureSrc;
-    directionImage.style.width = "50px";
-    directionImage.style.height = "auto";
-    cell3.appendChild(directionImage);
-    cell3.style.textAlign = "center";
-    cell3.style.verticalAlign = "middle";
+
+function handleUserGuess(pickedCountry, distance, directionPictureSrc, userGuessedCorrectly, country, difficulty, infoText, gameOver) {
+    var flag_part_imgs = document.querySelectorAll('img[class*="sub_image_"]');
+
+    if (userGuessedCorrectly === true) {
+        flag_part_imgs.forEach(function(imageElement) {
+            updateFlagPartImageSource(imageElement, country, difficulty)
+        })
+
+        var containerElement = document.querySelector('.container.mt-5');
+        containerElement.style.pointerEvents = 'none';
+
+        showUserText(infoText);
+    }
+    else if (gameOver === true) {
+        flag_part_imgs.forEach(function(imageElement) {
+            updateFlagPartImageSource(imageElement, country, difficulty)
+        })
+        var containerElement = document.querySelector('.container.mt-5');
+        containerElement.style.pointerEvents = 'none';
+        showUserText(infoText);
+    }
+    else {
+        var table = document.getElementById("user-guesses");
+        table.style.display = "table";
+
+        table = table.getElementsByTagName('tbody')[0];
+        var newRow = table.insertRow(table.rows.length);
+
+        var cell1 = newRow.insertCell(0);
+        var cell2 = newRow.insertCell(1);
+
+        cell1.innerHTML = pickedCountry;
+        cell2.innerHTML = distance;
+
+        cell1.style.textAlign = "center";
+        cell1.style.verticalAlign = "middle";
+
+        cell2.style.textAlign = "center";
+        cell2.style.verticalAlign = "middle";
+
+        var cell3 = newRow.insertCell(2);
+        var directionImage = document.createElement("img");
+        directionImage.src = directionPictureSrc;
+        directionImage.style.width = "50px";
+        directionImage.style.height = "auto";
+        cell3.appendChild(directionImage);
+        cell3.style.textAlign = "center";
+        cell3.style.verticalAlign = "middle";
+    }    
 }
 
 
